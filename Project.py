beafe24b8fe2970ec817f9b683e56d4a8bbc541d
aeyc <abegumtascioglu@gmail.com>
2019-11-26 13:25:03 +0300

    python 2.7 fix

--- Project.py
+++ Project.py
@@ -1,15 +1,16 @@
 from tkinter import*
 import random
-EDGE=20
+EDGE = 20
 X_LIMIT = 100
 Y_LIMIT = 100
+
 window = Tk()
 window.title("Work Place")
 canvas = Canvas(window, width =425, height=650)
-#frame = Frame(width=500, height=200, bg='blue')
-#canvas = Canvas(frame, bg='white')
+# frame = Frame(width=500, height=200, bg='blue')
+# canvas = Canvas(frame, bg='white')
 canvas.pack(fill = BOTH, expand = YES)
-#frame.pack(fill = BOTH, expand = YES)
+# frame.pack(fill = BOTH, expand = YES)
 
 
 def hex_points(x, y):
@@ -30,17 +31,17 @@ while i < 3:
     if choice == 1:
         x = random.randint(0, X_LIMIT)
         y = random.randint(EDGE, Y_LIMIT)
-        if (x,y) not in used_points:
-            hexagon_points = hex_points(x, y) #range of hexagon (x>=0, y>=20)
+        if (x, y) not in used_points:
+            hexagon_points = hex_points(x, y)   # range of hexagon (x>=0, y>=20)
             canvas.create_polygon(hexagon_points, outline='green', fill='yellow', width=1)
-            used_points.append((x,y))
+            used_points.append((x, y))   # used points needs fix
     elif choice == 0:
         x = random.randint(0, X_LIMIT)
         y = x
-        if (x,y) not in used_points:
+        if (x, y) not in used_points:
             s_points = square_points(x)
             canvas.create_polygon(s_points, outline="blue", fill="#fb0")
-            used_points.append((x,y))
+            used_points.append((x, y))
     i += 1
 
 window.mainloop()
