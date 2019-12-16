a7598e47125024208379572c2bc248fa49d36fdc
aeyc <abegumtascioglu@gmail.com>
2019-12-16 03:19:53 +0300

    fix

--- Project.py
+++ Project.py
@@ -25,8 +25,7 @@ def hex_area(x):
     return area
 
 
-def square_points(x):
-    y = x
+def square_points(x,y):
     points = (x, y, x, y+EDGE,x+EDGE,y+EDGE,x+EDGE,y)
     return points
 
@@ -40,7 +39,7 @@ appeared = False
 remaining_area = TOTAL_AREA
 moves = []
 used_points = []
-while count <15:
+while remaining_area >257000:
     choice = random.randint(0, 2)
     appeared = False
     tmp_points = []
@@ -48,15 +47,40 @@ while count <15:
         print("choice 1")
         x = random.randint(0, X_LIMIT)
         y = random.randint(EDGE, Y_LIMIT)
-        for i in range(x + EDGE, x - 1, -1):
-            for j in range(y + EDGE, y + EDGE - 1, -1):
+
+        #rectangular area
+        for i in range(x+EDGE, x+2*EDGE):
+            for j in range(y-EDGE, y+EDGE):
+                tmp_points.append((i,j))
+
+        #triangular area-up left
+        tmp_k = x
+        for j in range(y,y+EDGE):
+            for i in range(tmp_k, x+EDGE):
                 tmp_points.append((i, j))
-        for i in range(x + EDGE, x + 2 * EDGE + 1):
-            for j in range(y, y + 2 * EDGE + 1):
+            tmp_k+=1
+
+        # triangular area-down left
+        tmp_k = x
+        for j in range(y,y-EDGE,-1):
+            for i in range(tmp_k, x+EDGE):
                 tmp_points.append((i, j))
-        for i in range(x + 2 * EDGE, x + 3 * EDGE):
-            for j in range(y + 2 * EDGE, y + EDGE - 1, -1):
+            tmp_k+=1
+
+        # triangular area-up right
+        tmp_k = x+2*EDGE
+        for j in range(y, y + EDGE):
+            for i in range(tmp_k, x + 3*EDGE):
                 tmp_points.append((i, j))
+            tmp_k += 1
+
+        # triangular area-down right
+        tmp_k = x + 2 * EDGE
+        for j in range(y, y -EDGE,-1):
+            for i in range(tmp_k, x + 3 * EDGE):
+                tmp_points.append((i, j))
+            tmp_k += 1
+
         for i in used_points:
             for j in tmp_points:
                 if j == i:
@@ -65,39 +89,34 @@ while count <15:
             hexagon_points = hex_points(x, y)  # range of hexagon (x>=0, y>=20)
             a = canvas.create_polygon(hexagon_points, outline='green', fill='yellow', width=1)
             print("hex created")
-            for i in range(x+EDGE,x-1,-1):
-                for j in range(y+EDGE,y+EDGE-1,-1):
-                    used_points.append((i,j))
-            for i in range(x+EDGE, x+2*EDGE+1):
-                for j in range(y,y+2*EDGE+1):
-                    used_points.append((i, j))
-            for i in range(x+2*EDGE,x+3*EDGE):
-                for j in range(y+2*EDGE,y+EDGE-1,-1):
-                    used_points.append((i,j))
+            for i in tmp_points:
+                used_points.append(i)
             remaining_area -= hex_area(EDGE)
             moves.append((count, choice, (x, y)))
+            print("remaining area", remaining_area)
     elif choice == 0:
         print("choice 0")
         x = random.randint(0, X_LIMIT)
-        y = x
-        for i in range(x + EDGE, x - 1, -1):
-            for j in range(y + EDGE, y - 1, -1):
+        y = random.randint(EDGE, Y_LIMIT)
+        for i in range(x, x + EDGE):
+            for j in range(y, y + EDGE):
                 tmp_points.append((i, j))
         for i in used_points:
             for j in tmp_points:
                 if j == i:
                     appeared = True
         if not appeared:
-            print("not appeared")
-            s_points = square_points(x)
+            s_points = square_points(x,y)
             b = canvas.create_polygon(s_points, outline="blue", fill="#fb0")
             print("square created")
-            for i in range(x+EDGE,x-1,-1):
-                for j in range(y+EDGE,y-1,-1):
-                    used_points.append((i,j))
+            for i in range(x, x + EDGE):
+                for j in range(y, y + EDGE):
+                    used_points.append((i, j))
             remaining_area -= square_area(EDGE)
             moves.append((count,choice,(x,y)))
-        count+=1
+            print("remaining area", remaining_area)
+    count+=1
+    print("count",count)
     #canvas.delete("all")
 
 # count = 1
